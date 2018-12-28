c003c5ff5383209cd2684311fc179f9851db7012
seth350 <39222458+seth350@users.noreply.github.com>
2018-12-28 10:53:30 -0600

    Update linear_gauge.js
    Cleaned up the code and added comments

--- linear_gauge.js
+++ linear_gauge.js
@@ -13,10 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
- module.exports = function(RED) {
-    var settings = RED.settings;  
 
-    function HTML(config) {
+ 
+
+ /**
+  * REQUIRED
+  * A ui-node must always begin with the following function.
+  * module.exports = function(RED) {your code here}
+  */
+ module.exports = function(RED) {
+    var settings = RED.settings;  // not sure if this is needed? 
+
+
+    /**
+	 * REQUIRED
+     * A ui-node must always contain the following function.
+     * function HTML(config) {}
+     * This function will build the HTML necessary to display the lineargauge on the dashboard.
+     * It will also pass in the node's config so that the parameters may be referenced from the flow editor.
+     */
+    function HTML(config) { 
         var html = String.raw`
             <style>
                 .linearGauge1 {
@@ -67,8 +83,17 @@
         return html;
     };
 
-    var ui = undefined
+    var ui = undefined // instantiate a ui variable to link to the dashboard
     
+
+    /**
+	 * REQUIRED
+     * A ui-node must always contain the following function.
+     * function YourNodeNameHere(config){}
+     * This function will set the needed variables with the parameters from the flow editor.
+     * It also will contain any Javascript needed for your node to function. 
+     *  
+     */
     function LinearGaugeNode(config) {
          try {
              var node = this;
@@ -76,19 +101,22 @@
                 ui = RED.require("node-red-dashboard")(RED);
             }
             RED.nodes.createNode(this, config);
-            debugger;
 
-            var html = HTML(config);
-            var done = ui.addWidget({
-                node: node,
-                group: config.group,
-                width: config.width,
-                height: config.height,
-                format: html,
-                templateScope: "local",
-                emitOnlyNewValues: false,
-                forwardInputMessages: false,
-                storeFrontEndInputAsState: false,
+            // placing a "debugger;" in the code will cause the code to pause its execution in the web browser
+            // this allows the user to inspect the variable values and see how the code is executing
+            //debugger;                             
+
+            var html = HTML(config);                    // *REQUIRED* get the HTML for this node using the function from above
+            var done = ui.addWidget({                   // *REQUIRED* add our widget to the ui dashboard using the following configuration
+                node: node,                             // *REQUIRED*
+                group: config.group,                    // *REQUIRED*
+                width: config.width,                    // *REQUIRED*
+                height: config.height,                  // *REQUIRED*
+                format: html,                           // *REQUIRED*
+                templateScope: "local",                 // *REQUIRED*
+                emitOnlyNewValues: false,               // *REQUIRED*
+                forwardInputMessages: false,            // *REQUIRED*
+                storeFrontEndInputAsState: false,       // *REQUIRED*          
                 convertBack: function (value) {
                     return value;
                 },
@@ -100,52 +128,119 @@
                         return orig.msg;
                     }
                 },
+                /**
+                 * The initController is where most of the magic happens.
+                 * This is the section where you will write the Javascript needed for your node to function.
+                 * The 'msg' object will be available here.
+                 */
                 initController: function($scope, events) {
-                    debugger;
-
-                    
-                    
-                    $scope.flag = true;
+                    //debugger;
+                 
+                    $scope.flag = true;                                         // not sure if this is needed?
                     $scope.$watch('msg', function(msg) {
-                        var input = msg.payload
-                        var maxRange = msg.highlimit
-                        var minRange =  msg.lowlimit
-                        var minScale = 0
-                        var maxScale = 188 //this is the length of the gauge
-                        var setP =  msg.setpoint
 
-                        var diffH = maxRange - setP //find difference between setpoint and max limit
-                        var diffL = setP - minRange //find difference between setpoint and min limit
-                        delta = ((diffH+diffL)/2)   //calculate the mean to allow the same span above/below setpoint
+                        var payload = msg.payload
+                        var highLimit = msg.highlimit
+						var lowLimit=  msg.lowlimit
+						var setpoint =  msg.setpoint
+                        var gaugeStart = 0										// this is the gauge starting position, should be left at zero
+                        var gaugeEnd = 188                                      // this is the length of the gauge, if the gauge is to be longer, this value should be the sum of the heights of the scale areas
+						
+						
+						/**
+						 * The pointer is positioned within the scale container with the following equation.
+						 * Pointer Position = (msg.payload x Rate) + Offset
+						 * Rate = (Length Of Gauge - 0) / ((msg.lowlimit - delta) - (msg.highlimt + delta)
+						 * Offset = 0 - (msg.highlimit + delta) x Rate)
+						 * This equation will scale the msg.payload value into the gauge's length linearly. 
+						 */
 
-                        var sL = minRange - delta   //scaled low limit
-                        var sH = maxRange + delta   //scaled high limit
+                        var highDiff = highLimit - setpoint							//find difference between setpoint and the high limit
+						var lowDiff = setpoint - lowLimit 							//find difference between setpoint and the low limit
+						
+						/**
+						 * In order to span the the high and low areas of the gauge, we need to first calculate 
+						 * how much area to allow above and below the high and low limits.
+						 * The high limit starts where the middle area meets the top area.
+						 * The low limit starts where the middle area meets the bottom area.
+						 * 	    __
+						 *     |  | <- Gauge Start 
+						 * 	   |  |
+						 *     |__| <- High limit begins at this horizontal line
+						 *     |  | <-|
+						 *     |  |   |  Allowable  
+						 *     |  |   |    Range
+						 *     |  | <-|
+						 *     |__| <- Low limit begins at this horizontal line
+						 *     |  | 
+						 *     |  |
+						 *     |__| <- Gauge End
+						 * 	   
+						 * Without some extra room at either end, the pointer would peg out at
+						 * either end if the payload >= highLimit or if payload <= lowLimit.
+						 * Adding some cushion on either end of the gauge allows the user to visually
+						 * see how far above or below the setpoint range the payload is.
+						 */
 
-                        var rate = (maxScale - minScale)/(sL - sH) 
-                        var offset = minScale - (sH*rate)
+                        var delta = ( ( highDiff + lowDiff ) / 2 )   					//calculate the mean to allow the same span above/below setpoint area
 
-                        var value = (input*rate)+offset
+                        var lowSpan = lowLimit - delta   								//calculated low area span
+                        var highSpan = highLimit + delta   								//calulated high area span
 
-                        var ptr = document.getElementById("lgPtr_"+$scope.$eval('$id')) //get the pointer object
-                        var tt = document.getElementById("lgtooltip_"+$scope.$eval('$id')) //get the tooltip object
+                        var rate = ( gaugeEnd - gaugeStart ) / ( lowSpan - highSpan ) 
+                        var offset = gaugeStart - ( highSpan * rate )
 
-                        $(ptr).animate(
-                            {'foo':value},
+						var value = ( payload * rate ) + offset							//final scaled value should be between the value of gaugeStart and gaugeEnd
+						
+
+
+						/**
+						 * In order to reference an element within the HTML of the node, we must make a call
+						 * to the $scope to get the $id of the element we want to interact with. 
+						 * We do this by calling 
+						 *  $scope.$eval('$id')
+						 * This will return the unique identifier as a number to which is associated with
+						 * this particular node.
+						 * In order to interact with an element, we must also declare an object to reference
+						 * the element.
+						 * This is done by accessing the DOM via Javascript
+						 * 	document.getElementById("elementId_"+$scope.$eval('$id))
+						 * Note that in order for this to work, you must have also entered the element id
+						 * in the HTML. For example.
+						 * 	<div id="elementId_{{$id}}">
+						 * This is an Angular expression that will inject a unique ID number where ever you place {{$id}}. 
+						 * During creation of the ui node on the dashboard, only one ID number will be used during the 
+						 * time of creation. 
+						 */	
+
+						var ptr = document.getElementById("lgPtr_"+$scope.$eval('$id')) 					//get the pointer object
+						
+						$(ptr).animate(																		//animate the pointer
+                            {'ptrVal':value},																//get the final scaled value
                             {
-                                step: function(foo){
-                                    $(this).attr('transform', 'translate(10,'+foo+') rotate(90)');
+                                step: function(ptrVal){
+                                    $(this).attr('transform', 'translate(10,'+ptrVal+') rotate(90)');		//update the transform attribute with the new final scaled value
                                 },
-                                duration: 400 //set the duration of the sliding animation of the pointer
+                                duration: 400 																//sets the duration of the sliding animation of the pointer
                             }
                         );
-                        $(tt).html("HL: "+maxRange+"&#013;LL: "+minRange+"&#013;SP: "+setP);
+
+						var tt = document.getElementById("lgtooltip_"+$scope.$eval('$id')) 					//get the tooltip object
+						
+						$(tt).html("HL: "+highLimit+"&#013;SP: "+setpoint+"&#013;LL: "+lowLimit);			//set the tooltip to include the high/low/setpoint
+                        
                     });
                 }
             });
         }
         catch (e) {
-            console.log(e);
-        }
+            console.log(e);		// catch any errors that may occur and display them in the web browsers console
+		}
+		
+		/**
+		 * REQUIRED
+		 * I'm not sure what this does, but it is needed.
+		 */
         node.on("close", function() {
             if (done) {
                 done();
@@ -153,5 +248,10 @@
         });
     }
 
+
+	/**
+	 *  REQUIRED
+	 * Registers the node with a name, and a configuration.
+	 */
     RED.nodes.registerType("linear-gauge", LinearGaugeNode);
 }
